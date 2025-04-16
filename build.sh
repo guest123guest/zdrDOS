d8dfda684b9afc3b85e6d4fc6e3871f64d84a9ab
Zdrmonster Productions <zdrmonster@sdf.org>
2025-04-15 20:28:36 -0500

    Tried an AdLib emulation tester

--- build.sh
+++ build.sh
@@ -11,7 +11,9 @@ unzip HDPMI32i.zip
 wget https://github.com/Baron-von-Riedesel/Jemm/releases/download/v5.85/JemmB_v585.zip
 unzip JemmB_v585.zip
 wget https://github.com/crazii/SBEMU/releases/download/Release_1.0.0-beta.5/SBEMU.zip
-unzip -j /tmp/SBEMU.zip SBEMU/sbemu.exe
+unzip -j SBEMU.zip SBEMU/sbemu.exe
+wget https://vgmpf.com/Wiki/images/8/84/AdLib_%28DOS%29.zip
+unzip -j AdLib_(DOS).zip Originals/TEST.EXE
 cd ../
 mformat -i "hello.img" -B edrdos/image/bootfdos.144 -k -v "ZDRDOS" -f 1440 -N 0x306de779 -C
 mcopy -i "hello.img" edrdos/bin/kernel.sys ::/kernel.sys
@@ -23,3 +25,4 @@ mcopy -i "hello.img" temp/JLOAD.EXE ::/
 mcopy -i "hello.img" temp/sbemu.exe ::/
 mcopy -i "hello.img" dconfig.sys ::/
 mcopy -i "hello.img" dauto.bat ::/
+mcopy -i "hello.img" temp/TEXT.EXE ::/
